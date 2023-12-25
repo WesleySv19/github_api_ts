573112934614fb9fd7e5b7ccb5c9e06ffeb3245a
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-25 13:14:57 -0300

    configuring routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import Home from './routes/Home/index.tsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/", 
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+])
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
