4733e64a612fc70a99c544fe5a07f1410cd32a69
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-28 12:43:27 -0300

    adding enter event via keyboard

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,20 +3,28 @@ type SearchProps = {
 }
 
 import { BsSearch } from 'react-icons/bs'
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
+import styles from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
 
   const [userName, setUserName] = useState("")
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if(e.key === "Enter") {
+      loadUser(userName)
+    }
+  }
   return (
-    <div>
+    <div className={styles.search}>
         <h2>Busque por um usuário:</h2>
         <p>Conheça seus melhores repositórios</p>
 
-        <div>
+        <div className={styles.search_container}>
             <input type="text" 
             placeholder="Digite o nome do usuário" 
-            onChange={(e) => setUserName(e.target.value)} />
+            onChange={(e) => setUserName(e.target.value)} 
+            onKeyDown={handleKeyDown}/>
 
             <button onClick={() => loadUser(userName)}>
                 <BsSearch />
