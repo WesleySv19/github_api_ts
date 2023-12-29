7c349a4b9c378256f4012a09ba02ae16ba888dae
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-29 18:18:06 -0300

    adding style to the App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { Outlet } from "react-router-dom"
+import styles from './App.module.css'
 
 const App = () => {
 
   return (
-    <div>
+    <div className={styles.app}>
       <h1>GitHub Finder</h1>
       <Outlet />
     </div>
