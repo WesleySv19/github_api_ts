7fe78595ccb39043eadcdea87d855727df2406a2
Wesley da Silva <wesleyysilvaa1912@gmail.com>
2023-12-29 18:59:15 -0300

    showing the error

--- src/routes/Home/index.tsx
+++ src/routes/Home/index.tsx
@@ -1,19 +1,27 @@
 import { useState } from "react"
 import Search from "../../components/Search"
 import { UserProps } from "../../types/user"
+import User from "../../components/User"
+import Error from "../../components/Error"
 // import { useState } from "react"
 
 const Home = () => {
 
   const [user, setUser] = useState<UserProps | null>(null)
+  const [error, setError] = useState(false)
 
   const loadUser = async (userName: string) => {
+    setError(false)
+    setUser(null)
 
     const res = await fetch(`https://api.github.com/users/${userName}`)
-
     const data = await res.json()
-
     console.log(data)
+
+    if(res.status === 404) {
+      setError(true)
+      return
+    }
 
     const { avatar_url, login, location, followers, following } = data
 
@@ -31,7 +39,8 @@ const Home = () => {
   return (
     <div>
       <Search loadUser={loadUser}/>
-      {user && }
+      {user && <User {...user}/>}
+      {error && <Error />}
     </div>
   )
 }
